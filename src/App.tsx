8cc8d48f650ba94a02601fc72248a818599eb18e
softcoding03 <121743817+softcoding03@users.noreply.github.com>
2025-05-20 17:07:58 +0900

    Add interactive background effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import InteractiveBackground from './components/InteractiveBackground';
 
 function App() {
   return (
-    <>
-      <InteractiveBackground />
-      <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 flex items-center justify-center">
-        <p className="text-cyan-400">Start prompting (or editing) to see magic happen :)</p>
-      </div>
-    </>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p>Start prompting (or editing) to see magic happen :)</p>
+    </div>
   );
 }
 
